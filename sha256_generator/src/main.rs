3e8b900a911844c990ce712d99689b53a1d32ff5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-03-01 18:08:59 +0900

    cooking

--- sha256_generator/src/main.rs
+++ sha256_generator/src/main.rs
@@ -1,16 +1,26 @@
 use sha2::{Sha256, Sha512, Digest};
 use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::prelude::*;
+use std::os::raw;
 
 fn main(){
     let mut raw_data= String::new();
-    raw_data= "hellow world".to_string();
+    raw_data= getRawFileData();
     
-    let hex_256= generateSha256(&raw_data);
-    let hex_512= generateSha512(&raw_data);
+    // let hex_256= generateSha256(&raw_data);
+    // let hex_512= generateSha512(&raw_data);
 
-    
+    println!("{}", raw_data);
+}
+
+fn getRawFileData() -> String {
+    let filename= "1.txt";
+    let mut f= File::open(filename).expect("File not found");
+    let mut contents= String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    contents
 }
 
 fn generateSha256(data: &String) -> String{
