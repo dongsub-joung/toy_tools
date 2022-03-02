7a89630fc18a5df9a5f05817c182dfb30c77da5a
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-03-02 17:15:02 +0900

    toy project done

--- sha256_generator/src/main.rs
+++ sha256_generator/src/main.rs
@@ -1,5 +1,5 @@
 use sha2::{Sha256, Sha512, Digest};
-use std::env;
+use std::{env, string};
 use std::fs::File;
 use std::io::prelude::*;
 use std::os::raw;
@@ -11,31 +11,29 @@ fn main(){
     let mut raw_data= String::new();
     let names= getFileNames();
     for i in 0..names.len(){
-        raw_data= getRawFileData();
-
         let file_name= &names[i]; 
+        raw_data= getRawFileData(file_name.clone());
+        
         let hex_256= generateSha256(&raw_data);
         let hex_512= generateSha512(&raw_data);
     
         let writeable= format!("{} \n{}\n{}", file_name, hex_256, hex_512);
-        
-        // save a each "{i}.txt"
-        // 
+
+        let mut f= File::create("save.txt").expect("not found file in save");
+        f.write_all(writeable.as_bytes()).expect("err to write");
     }
-
-
-    println!("{}", raw_data);
 }
 
 fn getFileNames() -> Vec<String> {
     let mut v: Vec<String>= Vec::new();
+    
     // get the file name in directory, then set v
+    v.push("1.txt".to_string());
 
     v
 }
 
-fn getRawFileData() -> String {
-    let filename= "1.txt";
+fn getRawFileData(filename: String) -> String {
     let mut f= File::open(filename).expect("File not found");
     let mut contents= String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
