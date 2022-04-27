ccbef607b6a904d8262516c60eedd58f0ea5c5ce
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-04-27 10:57:56 +0900

    init

---
+++ Public-key-cryptography/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    const HARD: usize= 1212123121212;
+    let public_key= "abcd";
+    let private_key= "jds"; 
+    let plaintext= "Im_not_fuck_on_the_girl";
+
+    // 1. from str to byte
+
+    // 2. encrypto- plain
+    // 3. encrypto- public + plain(done) * HARD 
+    //
+}
