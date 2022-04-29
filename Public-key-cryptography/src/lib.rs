40d2b5b560c8364c7589bfc38d8298aba9aaf8b5
jds2 <jds2@pop-os.localdomain>
2022-04-30 00:30:06 +0900

    done

--- Public-key-cryptography/src/lib.rs
+++ Public-key-cryptography/src/lib.rs
@@ -2,7 +2,7 @@ use std::str::Bytes;
 use sha2::{Sha256, Sha512, Digest};
 
 #[cfg(test)]
-fn hash_generator() -> {
+fn hash_generator() -> String{
     const HARD: &'static str= "String";
     let public_key= "abcd";
     let private_key= "jds"; 
@@ -23,6 +23,13 @@ mod testing{
 
     #[test]
     fn test1(){
-
+        // The aother
+        let mut hasher= Sha256::new();
+        let plain= "String_Im_not_fuck_on_the_girl";
+        hasher.update(plain.as_bytes());
+        let mut result= hasher.finalize();
+        let end= hex::encode(&result);
+        
+        assert_eq!(end, hash_generator());
     }
 }
