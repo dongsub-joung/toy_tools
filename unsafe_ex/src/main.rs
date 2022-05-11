a17c01274f4b8947549f60d483c99f6630568ace
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-05-11 16:03:22 +0900

    unsafe

--- unsafe_ex/src/main.rs
+++ unsafe_ex/src/main.rs
@@ -1,3 +1,28 @@
+use std::slice;
+
+unsafe fn dangerous(r1: *const i32, r2: *mut i32){
+    println!("r1 {}", *r1);
+    println!("r2 {}", *r2);
+}
+
+fn splite_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]){
+    let len= slice.len();
+    let ptr= slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid);
+        )
+    }
+}
+
+extern "C"{
+    fn abs(input: i32) -> i32;
+}
+
 fn main() {
     let mut num= 5;
 
@@ -5,7 +30,10 @@ fn main() {
     let r2= &mut num as *mut i32;
     
     unsafe{
-        println!("r1", *r1);
-        println!("r2", *r2);
+        dangerous(r1, r2);
+    }
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
     }
 }
