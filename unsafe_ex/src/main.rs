df15575a97647a2d23e6f2752b5b79c7416d8d81
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-05-13 21:01:36 +0900

    unsafe

--- unsafe_ex/src/main.rs
+++ unsafe_ex/src/main.rs
@@ -1,3 +1,5 @@
+// https://doc.rust-lang.org/book/ch19-01-unsafe-rust.html
+
 use std::slice;
 
 unsafe fn dangerous(r1: *const i32, r2: *mut i32){
@@ -23,6 +25,14 @@ extern "C"{
     fn abs(input: i32) -> i32;
 }
 
+// Accessing or Modifying a Mutable Static Variable
+static mut COUNTER: u32= 0;
+fn add_to_count(inc: u32) {
+    unsafe{
+        COUNTER += inc;
+    }
+}
+
 fn main() {
     let mut num= 5;
 
@@ -35,5 +45,11 @@ fn main() {
 
     unsafe {
         println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+
+    // Accessing or Modifying a Mutable Static Variable
+    add_to_count(3);
+    unsafe{
+        println!("COUNTER: {}", COUNTER);
     }
 }
